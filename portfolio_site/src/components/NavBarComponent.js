702227dd9bd956c9a75f6aed56bdefa449191fda
jamescross813 <crossjames813@gmail.com>
2022-01-06 17:46:27 -0600

    name change

--- portfolio_site/src/components/NavBarComponent.js
+++ portfolio_site/src/components/NavBarComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBarComponent = () => {
   return (
     <div className="navbar">
         <NavLink 
