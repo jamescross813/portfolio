57ccddf99501c835033ae7e3d881633532769448
jamescross813 <crossjames813@gmail.com>
2022-01-06 17:10:37 -0600

    basic screenshot class added

---
+++ portfolio_site/src/components/ScreenShotComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ScreenShotComponent extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+} 
+
+export default ScreenShotComponent
