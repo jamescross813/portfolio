2046eedd6ad0a15bf2d65d9c6075fc01b4e367c1
jamescross813 <crossjames813@gmail.com>
2022-01-06 17:45:35 -0600

    header working

--- portfolio_site/src/components/HeaderComponent.js
+++ portfolio_site/src/components/HeaderComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import header from "../planets-drawing-facebook-cover.jpg"
 
 class HeaderComponent extends Component{
     render(){
         return(
             <div>
-                
+                <img src={header} className="header" alt="header" />
             </div>
         )
     }
