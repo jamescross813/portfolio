cd02245723b3e1079e1c323f5709db086b3620b1
jamescross813 <crossjames813@gmail.com>
2022-01-06 17:18:18 -0600

    basic outline for NavBar

--- portfolio_site/src/components/NavBarComponent.js
+++ portfolio_site/src/components/NavBarComponent.js
@@ -4,7 +4,40 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-     
+        <NavLink 
+            to="/"
+            exact
+            >
+            Home
+        </NavLink>
+        <NavLink
+            to="/about"
+            exact
+            >
+            About
+        </NavLink>
+        <NavLink 
+            to="/resume"
+            >
+            Resume
+        </NavLink>
+        <NavLink 
+            to="/github"
+            exact
+            >
+            GitHub
+        </NavLink>
+        <NavLink
+            to="/LinkedIn"
+            >
+            LinkedIn    
+            </NavLink>
+        <NavLink 
+        to="/projects"
+        exact
+        >
+          Projects
+        </NavLink>
     </div>
   );
 };
