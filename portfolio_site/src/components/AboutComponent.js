0d8ce44f4fec5056861c1fa09a2a1bef853ef965
jamescross813 <crossjames813@gmail.com>
2022-01-06 18:17:02 -0600

    basic about me

--- portfolio_site/src/components/AboutComponent.js
+++ portfolio_site/src/components/AboutComponent.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 class AboutComponent extends Component{
     render(){
         return(
-            <div>
-
+            <div classname = "about-me">
+                <h1>About</h1>
+                <p>Hi! My name is James Cross. i'm a Geek, a Nerd and a Coder.</p>
+                <p>I've worn many hats over my career span and am excited for the challenges that come with my new path.</p>
             </div>
         )
     }
