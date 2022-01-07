15aca7c66c174cdf97ae3ef5cdd57162b83eff94
jamescross813 <crossjames813@gmail.com>
2022-01-06 18:02:51 -0600

    name change

--- portfolio_site/src/components/BlogsComponent.js
+++ portfolio_site/src/components/BlogsComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class BlogComponent extends Component{
+class BlogsComponent extends Component{
     render(){
         return(
             <div>
@@ -10,4 +10,4 @@ class BlogComponent extends Component{
     }
 } 
 
-export default BlogComponent
+export default BlogsComponent
