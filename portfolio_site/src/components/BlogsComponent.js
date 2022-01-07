eb0150ce6d1367eb44ae26322269f3fb82877f2c
jamescross813 <crossjames813@gmail.com>
2022-01-06 18:24:23 -0600

    const set up

--- portfolio_site/src/components/BlogsComponent.js
+++ portfolio_site/src/components/BlogsComponent.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const BlogsComponent = () => {
-
+    
+    const waffle = {
+        starting: "First Blog",
+        firstProject: "CLI Project",
+        secondProject: "Sinatra Project",
+        thirdProject: "Rails Project",
+        fourthProject: "JS Project",
+        fifthProject: "React Project",
+        ending: "Next steps blog"
+    }
     
         return(
             <div>
