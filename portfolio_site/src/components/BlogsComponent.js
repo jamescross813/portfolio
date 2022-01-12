5dbf510b46c3dc3a4821e1fade57a174b4a8d4b9
jamescross813 <crossjames813@gmail.com>
2022-01-11 20:11:52 -0600

    simplified blog links to array

--- portfolio_site/src/components/BlogsComponent.js
+++ portfolio_site/src/components/BlogsComponent.js
@@ -14,13 +14,13 @@ const BlogsComponent = () => {
     ]
 
     let blogLinks = [
-        {1: "https://crossjames813.wixsite.com/my-site/post/day-one-why-and-other-questions"},
-        {2: "https://crossjames813.wixsite.com/my-site/post/cheapgeek-cli-app-from-idea-to-reality"},
-        {3: "https://crossjames813.wixsite.com/my-site/post/sinatra-project-a-day-in-the-life-of-a-fool"},
-        {4: "https://crossjames813.wixsite.com/my-site/post/riding-the-rails"},
-        {5: "https://crossjames813.wixsite.com/my-site/post/javascript-dom-manipulation-and-other-adventures"},
-        {6: "https://crossjames813.wixsite.com/my-site/post/react-redux-a-tale-of-two-frameworks"},
-        {7: "https://crossjames813.wixsite.com/my-site/post/journey-s-end-marathon-s-beginning"}
+        "https://crossjames813.wixsite.com/my-site/post/day-one-why-and-other-questions",
+        "https://crossjames813.wixsite.com/my-site/post/cheapgeek-cli-app-from-idea-to-reality",
+        "https://crossjames813.wixsite.com/my-site/post/sinatra-project-a-day-in-the-life-of-a-fool",
+        "https://crossjames813.wixsite.com/my-site/post/riding-the-rails",
+        "https://crossjames813.wixsite.com/my-site/post/javascript-dom-manipulation-and-other-adventures",
+        "https://crossjames813.wixsite.com/my-site/post/react-redux-a-tale-of-two-frameworks",
+        "https://crossjames813.wixsite.com/my-site/post/journey-s-end-marathon-s-beginning"
     ]
     
     let renderBlog = (blogInfo)=>{
