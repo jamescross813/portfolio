ff7f0cc106a2e53120b9b551d96f6eeaef65885f
jamescross813 <crossjames813@gmail.com>
2022-01-12 23:23:22 -0600

    formatting

--- portfolio_site/src/components/ProjectsComponent.js
+++ portfolio_site/src/components/ProjectsComponent.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
 const ProjectsComponent = ()=>{
-
+    
 
     
-        return(
-            <div>
-                {/* include screen shots and descriptions */}
-            </div>
-        )
+    return(
+        <div>
+            {/* include screen shots and descriptions */}
+        </div>
+    )
     
 } 
 
