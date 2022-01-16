5e3ac4821f0852d8a5b871ae00b80d602d963d0a
jamescross813 <crossjames813@gmail.com>
2022-01-16 11:10:29 -0600

    removed comments

--- portfolio_site/src/components/ProjectsComponent.js
+++ portfolio_site/src/components/ProjectsComponent.js
@@ -22,7 +22,6 @@ const ProjectsComponent = ()=>{
     return(
         <div>
             {renderProjectInfo(projectTitle)}
-            {/* include screen shots and descriptions */}
         </div>
     )
     
