e79034518621ec21f30d8cfa7732ed7264854f21
jamescross813 <crossjames813@gmail.com>
2022-01-17 13:33:12 -0600

    classname added

--- portfolio_site/src/components/AboutComponent.js
+++ portfolio_site/src/components/AboutComponent.js
@@ -4,9 +4,9 @@ class AboutComponent extends Component{
     render(){
         return(
             <div classname = "about-me">
-                <h1>About</h1>
-                <p>Hi! My name is James Cross. I'm a Geek, a Nerd and a Coder.</p>
-                <p>I've worn many hats over my career span and am excited for the challenges that come with my new path.</p>
+                <h1 className="about-me-title">About</h1>
+                <p className="about-me-line-one">Hi! My name is James Cross. I'm a Geek, a Nerd and a Coder.</p>
+                <p className="about-me-line-two">I've worn many hats over my career span and am excited for the challenges that come with my new path.</p>
             </div>
         )
     }
